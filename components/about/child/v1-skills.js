ce0d618d2823b2cb586d979189436f58464cf716
Cameron Roberts <44409341+koalabur@users.noreply.github.com>
2022-12-04 16:31:24 -0800

    switch to firestore from realtime databse (#4)

--- components/about/child/v1-skills.js
+++ components/about/child/v1-skills.js
@@ -1,46 +1,34 @@
 import { useState, useEffect } from "react";
 
-// Axios
-import axios from "axios";
+import { db } from "/firebase.config.js";
+import { collection, getDocs } from "firebase/firestore";
 
 import styles from "/styles/about/v1.module.scss";
 
 // Loops through about items from parent
 export default function AboutItems() {
     const [skillsData, setSkillsData] = useState([]);
+    const skillsDb = collection(db, "skills");
 
-    const getSkillsData = async () => {
-        await axios
-            // Get the goods
-            .get(
-                "https://portfolio-96dd5-default-rtdb.firebaseio.com/skills.json"
-            )
-            .then((res) => {
-                // Organize values from least to greatest
-                let resData = Object.entries(res.data)
-                    .sort((a, b) => a[1] - b[1])
-                    // Items were put inside another array. Need to take them out
-                    .map((item) => item[0]);
-
-                // Keys and values were flipped when sorting from least to greatest
-                // ex. Firebase: key: html, value: 1
-                // ex. Here: key: 1, value: html
-                setSkillsData(Object.values(resData));
-            })
-            .catch((error) => console.log(error));
-    };
-
-    // For the love of everything keep the useEffect hook
-    // this will run until the end of time and blow up the internet w/o it
     useEffect(() => {
-        getSkillsData();
+        const getSkills = async () => {
+            try {
+                const data = await getDocs(skillsDb);
+                const newData = data.docs.map((doc) => ({ ...doc.data() }));
+                setSkillsData(newData[0].items);
+            } catch {
+                console.warn("Data couldn't be fetched from firebase");
+            }
+        };
+
+        getSkills();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     // Do the loop thing
-    const skillsItems = skillsData.map((item, index) => {
+    const skillsItems = skillsData.map((item) => {
         return (
-            <p className={styles["about__skills-item"]} key={index}>
+            <p className={styles["about__skills-item"]} key={item}>
                 {item}
             </p>
         );
