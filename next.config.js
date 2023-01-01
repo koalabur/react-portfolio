3e1d8ac726678f05026fd45efe4e7cb6b9580fbf
Cameron Roberts <44409341+koalabur@users.noreply.github.com>
2023-01-01 08:43:09 -0800

    mirroring vue-portfolio release version 1.1.0 (#6)
    * mirroring vue-portfolio release version 1.1.0
    
    * update .env.local.example

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles/scss")],
+    prependData: `@import "app.scss";`,
+  },
+  images: {
+    loader: "cloudinary",
+    path: "https://res.cloudinary.com/dg6sd9yyx/image/upload/v1671834530",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,27 @@
-import React from "react"
-import { CurrentSlideContext } from '../components/portfolio/context/v1'
-import '/styles/globals.scss'
+import React from "react";
+import { CurrentSlideContext } from "../components/portfolio/context/v1";
+import "/styles/scss/globals.scss";
 
-import { IBM_Plex_Sans } from '@next/font/google'
+import { IBM_Plex_Sans } from "@next/font/google";
 
 const gfont = IBM_Plex_Sans({
   weight: ["200", "400", "600"],
   preload: true,
   subsets: ["latin"],
   fallback: ["arial"],
-  variable: '--main-font'
-})
+  variable: "--main-font",
+});
 
 function MyApp({ Component, pageProps }) {
-  const [current, setCurrent] = React.useState(0)
+  const [current, setCurrent] = React.useState(0);
 
   return (
-    <main className={gfont.variable}>
+    <div className={gfont.variable}>
       <CurrentSlideContext.Provider value={{ current, setCurrent }}>
         <Component {...pageProps} />
       </CurrentSlideContext.Provider>
-    </main>
-  )
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
