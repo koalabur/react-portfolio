77c1910365feeff6c38dc3c0eb35ee5e7a75f54a
Cameron Roberts <44409341+koalabur@users.noreply.github.com>
2023-01-10 17:37:31 -0800

    scroll offset to #about. format updates to code (#14)

--- context/GlobalState.js
+++ context/GlobalState.js
@@ -1,21 +1,30 @@
 import { useState, createContext } from "react";
 
 const AppContext = createContext({
-  slide: '',
+  slide: "",
   setSlide: () => {},
-  section: '',
+  section: "",
   setSection: () => {},
-  isSiteReady: '',
-  setIsSiteReady: () => {}
+  isSiteReady: "",
+  setIsSiteReady: () => {},
 });
 
 const AppContextProvider = (props) => {
   const [slide, setSlide] = useState(0);
   const [section, setSection] = useState("");
-  const [isSiteReady, setIsSiteReady] = useState(false)
+  const [isSiteReady, setIsSiteReady] = useState(false);
 
   return (
-    <AppContext.Provider value={{ slide, setSlide, section, setSection, isSiteReady, setIsSiteReady }}>
+    <AppContext.Provider
+      value={{
+        slide,
+        setSlide,
+        section,
+        setSection,
+        isSiteReady,
+        setIsSiteReady,
+      }}
+    >
       {props.children}
     </AppContext.Provider>
   );
@@ -23,13 +32,12 @@ const AppContextProvider = (props) => {
 
 export { AppContext, AppContextProvider };
 
-
 //* Usage
 
 // Context
 // Imported at top of file
 // import { AppContext } from "../../context/GlobalState";
 
-// Global state 
+// Global state
 // Use same way you would local state
 // const { slide, setSlide, section, setSection, isSiteReady, setIsSiteReady } = useContext(AppContext);
