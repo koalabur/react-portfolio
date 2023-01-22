09772502efa4d95a0b075e83d7ab569d16fbd0b4
Cameron Roberts <cameronrdesign@gmail.com>
2023-01-22 12:47:20 -0800

    add new fullscreen preloader

---
+++ components/preLoader/Fullscreen-Preloader.js
@@ -0,0 +1,61 @@
+// React
+import { useEffect, useContext, useState } from "react";
+
+// Context
+import { AppContext } from "../../context/GlobalState";
+
+// Styles
+import styles from "/styles/preLoader/fullscreen-preloader.module.scss";
+
+export default function PreLoader() {
+  // Global State
+  const { isSiteReady } = useContext(AppContext);
+
+  // Local State
+  const [mssg, setMssg] = useState([
+    "Loading site...",
+    "Removing stains...",
+    "99.9999% complete...",
+    "Doing a live NPM install.",
+    "I promise my other loading screen is much faster.",
+    "I feel like im supposed to be loading something...",
+    "Kicking the router.",
+  ]);
+  const [mssgNum, setMssgNum] = useState(0);
+  const [showMssg, setShowMssg] = useState(false);
+
+  function randomMssg() {
+    const length = mssg.length;
+    setMssgNum(Math.floor(Math.random() * length));
+  }
+
+  // onMount
+  useEffect(() => {
+    randomMssg();
+    setShowMssg(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // onStateUpdate and onMount
+  useEffect(() => {
+    !isSiteReady
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  }, [isSiteReady]);
+
+  return (
+    <section className={styles.fullscreenPreloader}>
+      <p
+        className={`${styles.fullscreenPreloader__title} ${
+          !showMssg ? styles["fullscreenPreloader__title--hide"] : null
+        }`}
+      >
+        {mssg[mssgNum]}
+      </p>
+
+      <div className={styles.fullscreenPreloader__preloader}>
+        <div></div>
+      </div>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+// React
+import { useContext } from "react";
+
 // Next
 import Head from "next/head";
 
+// Framer motion
+import { AnimatePresence, motion } from "framer-motion";
+
+// Context
+import { AppContext } from "../context/GlobalState";
+
 // Components
+import Preloader from "../components/preloader/Fullscreen-Preloader";
 import Sidebar from "../components/sidebar/v2";
 import Nav from "../components/nav/v1";
 import Intro from "../components/intro/v1";
@@ -10,6 +20,9 @@ import Portfolio from "../components/portfolio/v1";
 import Footer from "../components/footer/v1";
 
 export default function Home() {
+  // Global state
+  const { isSiteReady } = useContext(AppContext);
+
   return (
     <>
       <Head>
@@ -19,6 +32,21 @@ export default function Home() {
       </Head>
 
       {/* Components below */}
+      <AnimatePresence>
+        {!isSiteReady ? (
+          <motion.div
+            animate={{
+              position: "fixed",
+              zIndex: 25,
+              width: "100%",
+              height: "100%",
+            }}
+            exit={{ opacity: 0 }}
+          >
+            <Preloader />
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
       <Nav />
       <main>
         <Intro />
