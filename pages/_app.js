4666e44d91415f49cc698469b29b94ef225d73cf
Cameron Roberts <44409341+koalabur@users.noreply.github.com>
2023-02-10 09:04:15 -0800

    unique visitor count (backend) (#24)
    * unique visitor count
    
    * sameSite: true
    
    * separate dev and prod

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import React from "react";
+// React
+import React, { useEffect } from "react";
+
+// Hook
+import { UseUpdateUserCount } from "../hooks/useGetFirestore";
+
+// Cookies
+import { setCookie, getCookie } from "cookies-next";
+
+// Global State
 import { AppContextProvider } from "../context/GlobalState";
+
+// Global Vars
 import "/styles/scss/globals.scss";
 
 import { IBM_Plex_Sans } from "@next/font/google";
@@ -16,6 +27,37 @@ const gfont = IBM_Plex_Sans({
 });
 
 function MyApp({ Component, pageProps }) {
+  // This function will get the user's IP and set it as a key (CRD_UNIQUE_USER_REACT) value (their ip) in the cookies
+  const expIn30Days = 2592000;
+  async function getIp() {
+    if (!getCookie("CRD_UNIQUE_USER_REACT")) {
+      try {
+        // Get Data
+        const data = await fetch(
+          `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.IPGEO_KEY}`
+        );
+        // Get Result
+        const result = await data.json();
+        // Set IP in cookies
+        setCookie("CRD_UNIQUE_USER_REACT", result.ip, {
+          maxAge: expIn30Days,
+          sameSite: true,
+        });
+        // Compare cookie IP with API IP
+        if (getCookie("CRD_UNIQUE_USER_REACT") === result.ip) {
+          // Update firestore + 1
+          UseUpdateUserCount("site_visits", "react");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+  // Do everything above after component renders
+  useEffect(() => {
+    getIp();
+  }, []);
+
   return (
     <div className={gfont.variable}>
       <AppContextProvider>
