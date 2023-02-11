1e17c72339a127fc6c4d11faaf6784d7fb153ad9
Cameron Roberts <cameronrdesign@gmail.com>
2023-02-11 10:39:55 -0800

    GA and meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
-// React
-import { useContext } from "react";
-
 // Next
 import Head from "next/head";
-
-// Context
-import { AppContext } from "../context/GlobalState";
 
 // Components
 import Sidebar from "../components/sidebar/FullHeightSidebar";
@@ -16,13 +10,27 @@ import Portfolio from "../components/portfolio/GSAPPortfolio";
 import Footer from "../components/footer/BasicFooter";
 
 export default function Home() {
-  // Global state
   return (
     <>
       <Head>
         <title>Cameron R Design</title>
-        <meta name="description" content="React/ Next.js Portfolio" />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="React/ Next.js Portfolio for Cameron Roberts"
+        />
+        <meta
+          property="og:title"
+          content="React/ Next.js Portfolio for Cameron Roberts"
+        />
+        <meta
+          property="og:description"
+          content="React/ Next.js Portfolio for Cameron Roberts"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dg6sd9yyx/image/upload/v1676137078/meta/koalareact-ogimage_ibr68g.jpg"
+        />
       </Head>
 
       {/* Components below */}
