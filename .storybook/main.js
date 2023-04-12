884ba5a49dd5b79a9e1456bc7249de61b3471a4b
Cameron Roberts <44409341+koalabur@users.noreply.github.com>
2023-04-11 17:14:51 -0700

    Update storybook (#34)
    * chore: update storybook
    
    * chore: update package-lock

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,7 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
+  "stories": ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   "framework": {
     "name": "@storybook/nextjs",
     "options": {}
